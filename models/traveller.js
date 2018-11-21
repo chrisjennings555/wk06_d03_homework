75ae1a5415276ddef62b18ca1de6a7017b434180
Chris Jennings <chrisjennings555@gmail.com>
2018-11-21 17:29:39 +0000

    upto total distance

--- models/traveller.js
+++ models/traveller.js
@@ -13,22 +13,31 @@ Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map((journey) => {
     const journeyEnds = journey.endLocation;
     return journeyEnds;
-})
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
   return this.journeys.filter((vehicle) => {
     return vehicle.transport === transport;
-
   })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
 
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const allDistances = this.journeys.map((journey) => {
+    const distances = journey.distance;
+    return distances;
+  })
+  const total = allDistances.reduce((accum, distance) => {
+    return accum += distance;
+  }, 0);
+  return total;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
